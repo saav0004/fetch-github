1c15785f7005bbc58ceb1ec5bb59812455e5ac6b
Luciano Saavedra <saav0004@algonquinlive.com>
2022-11-14 13:03:00 -0500

    working on fetch-github

---
+++ main.js
@@ -0,0 +1,46 @@
+//select .btn from html
+let btn = document.querySelector(".btn");
+
+//event
+btn.addEventListener("click", getUser);
+
+//https://api.github.com/users/{username}/repos
+
+function getUser(ev) {
+  let input = document.getElementById("username").value.trim();
+  let url = `https://api.github.com/users/${input}/repos`;
+  let repoUL = document.querySelector(".follower-info");
+  let followerUL = document.querySelector(".user-info");
+  let df = new DocumentFragment();
+
+  if (input === "") {
+    console.log("not a valid input");
+  } else {
+    repoUL.innerHTML = "";
+    fetch(url)
+      .then((response) => response.json())
+      .then((obj) => {
+        console.log(obj);
+        obj.forEach((element) => {
+          const li = document.createElement("li");
+          li.innerHTML = `<p>
+          <a>${element["name"]}</a>
+          </p>
+          <p>Watchers: ${element["watchers"]}</p>
+          <p>Open Issues: ${element["open_issues"]}</p>`;
+          df.append(li);
+        });
+        repoUL.append(df);
+      })
+      .catch((err) => {
+        let nm = err.name;
+        let msg = err.message;
+        alert(`CATCH: ${nm} ${msg}`);
+      });
+  }
+}
+
+
+//h.append(name, value)
+//usp = new URLSearchParams()
+//usp.append(name, value)
