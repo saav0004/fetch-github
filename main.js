d085e13b7021e89adfeec933614fb84b22c05d41
Luciano Saavedra <saav0004@algonquinlive.com>
2022-11-17 21:59:20 -0500

    completed fetch-github assignment

--- main.js
+++ main.js
@@ -1,39 +1,53 @@
-//select .btn from html
-let btn = document.querySelector(".btn");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".content__input");
 
-//event
 btn.addEventListener("click", getUser);
 
 function getUser(ev) {
-  let input = document.getElementById("username").value.trim();
-  let url = `https://api.github.com/users/${input}/repos`;
-  let repoUL = document.querySelector(".user-repos");
-  let df = new DocumentFragment();
+  const inputValue = document.getElementById("username").value.trim();
+  input.value = inputValue;
+  const url = `https://api.github.com/users/${inputValue}/repos`;
+  const repoUL = document.querySelector(".user-repos");
+  const noRepo = document.querySelector(".no-repos");
+  const df = new DocumentFragment();
 
-  if (input === "") {
-    alert("Not a valid input");
+  if (inputValue === "") {
+    noRepo.innerHTML = "";
+    repoUL.innerHTML = "";
+    h2Message = document.createElement("h2");
+    h2Message.innerHTML = `Please provide a username in the field`;
+    df.append(h2Message);
+    noRepo.append(df);
   } else {
+    noRepo.innerHTML = "";
     repoUL.innerHTML = "";
     fetch(url)
       .then((response) => response.json())
       .then((obj) => {
-        console.log(obj);
-        obj.forEach((element) => {
-          const li = document.createElement("li");
-          li.innerHTML = `
+        if (obj.length === 0) {
+          h2Message = document.createElement("h2");
+          h2Message.innerHTML = `User "${inputValue}" does not have any public repositories...try another username.`;
+          df.append(h2Message);
+          noRepo.append(df);
+        } else {
+          obj.forEach((element) => {
+            const li = document.createElement("li");
+            li.innerHTML = `
           <p class="content__title">
           <a class="content__a" href="${element["html_url"]}" target="_blank" >${element["name"]}</a>
           </p>
           <p class="content__p">Watchers: ${element["watchers"]}</p>
           <p  class="content__p">Open Issues: ${element["open_issues"]}</p>`;
-          df.append(li);
-        });
-        repoUL.append(df);
+            df.append(li);
+          });
+          repoUL.append(df);
+        }
       })
       .catch((err) => {
-        let nm = err.name;
-        let msg = err.message;
-        alert(`CATCH: ${nm} ${msg}`);
+        h2Message = document.createElement("h2");
+        h2Message.innerHTML = `Oops! Looks like that is not a valid username...try again,`;
+        df.append(h2Message);
+        noRepo.append(df);
       });
   }
 }
